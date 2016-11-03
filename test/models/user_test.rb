d93643176ff6e25b2b14375321697f7b07fcb5f1
Joseph Ferrairo <josephferrairo@gmail.com>
2016-11-03 15:09:26 -0400

    set up posts, refactored user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,17 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:valid)
+  end
 
   test 'valid user' do
-
+    assert @user.valid?
   end
 
   test 'invalid without name' do
-
+    @user.name = nil
+    refute @user.valid?, 'user is valid without a name'
+    assert_not_nil @user.errors[:name], 'no validation error for name present'
   end
 
   test 'invalid without email' do
-
+    @user.email = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:email]
   end
-  
+
+  test '#posts' do
+      assert_equal 2, @user.posts.size
+  end
+
 end
