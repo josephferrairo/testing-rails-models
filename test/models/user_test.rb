537f1e637cedaa836465d38a8ac07584cc2827a4
Joseph Ferrairo <josephferrairo@gmail.com>
2016-11-03 14:57:26 -0400

    fixed guard so it works with spring

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'valid user' do
+
+  end
+
+  test 'invalid without name' do
+
+  end
+
+  test 'invalid without email' do
+
+  end
+  
 end
